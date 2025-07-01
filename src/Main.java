06ae001f768c3b99389a34fe29482e208f538f0b
nika <nika01s555@gmail.com>
2025-07-01 12:17:25 +0500

    выполнила задание вместо с++ на Java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,106 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.Math;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void task1() {
+        int n = 10;
+        int m = 4;
+        double I = n * (Math.log(m) / Math.log(2));
+        System.out.println("Количество информации: " + I + " бит");
+    }
+
+    public static void task2() {
+        int m = 8;
+        double bits = Math.log(m) / Math.log(2);
+        System.out.println("Количество бит для кодирования одного символа: " + bits);
+    }
+
+    public static void task3() {
+        int k = 5;
+        int m = (int) Math.pow(2, k);
+        System.out.println("Максимальное количество символов, кодируемых 5 битами: " + m);
+    }
+
+    public static void task4() {
+        int maxnumber = 31;
+        int totalnumbers = maxnumber + 1;
+        int k = 0;
+        int power = 1;
+        while (power < totalnumbers) {
+            power *= 2;
+            k++;
         }
+        System.out.println("Длина кода в битах: " + k);
+    }
+
+    public static void task5() {
+        double p0 = 0.5;
+        double p1 = 0.5;
+        double H = 0;
+        if (p0 > 0) H += p0 * (Math.log(p0) / Math.log(2));
+        if (p1 > 0) H += p1 * (Math.log(p1) / Math.log(2));
+        H = -H;
+        System.out.println("Энтропия источника: " + H + " бит");
+    }
+
+    public static void task6() {
+        double pA = 0.7;
+        double pB = 0.2;
+        double pC = 0.1;
+        double H = 0;
+        if (pA > 0) H += pA * (Math.log(pA) / Math.log(2));
+        if (pB > 0) H += pB * (Math.log(pB) / Math.log(2));
+        if (pC > 0) H += pC * (Math.log(pC) / Math.log(2));
+        H = -H;
+        System.out.println("Энтропия источника: " + H + " бит");
+    }
+
+    public static void task7() {
+        int m = 33;
+        double I = Math.log(m) / Math.log(2);
+        System.out.println("Информация, которую несёт одна буква: " + I + " бит");
+    }
+
+    public static void task8() {
+        double pA = 0.5, pB = 0.3, pC = 0.2;
+        int lA = 1, lB = 2, lC = 3;
+        double L = pA * lA + pB * lB + pC * lC;
+        double H = 0;
+        if (pA > 0) H += pA * (Math.log(pA) / Math.log(2));
+        if (pB > 0) H += pB * (Math.log(pB) / Math.log(2));
+        if (pC > 0) H += pC * (Math.log(pC) / Math.log(2));
+        H = -H;
+        System.out.println("Средняя длина кода: " + L + " бит");
+        System.out.println("Энтропия источника: " + H + " бит");
+        if (L >= H)
+            System.out.println("Кодирование эффективно (средняя длина кода >= энтропии)");
+        else
+            System.out.println("Кодирование неэффективно (средняя длина кода < энтропии)");
+    }
+
+    public static void task9() {
+        int m = 3;
+        int n = 6;
+        int N = (int) Math.pow(m, n);
+        System.out.println("Количество различных сообщений длины " + n + ": " + N);
+    }
+
+    public static void task10() {
+        int n = 26;
+        int m = 5;
+        double I = n * (Math.log(m) / Math.log(2));
+        System.out.println("Количество информации: " + I + " бит");
     }
 }
